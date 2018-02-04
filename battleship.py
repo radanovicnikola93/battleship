1754b7e07c49dcca36de68a77360a2be237d699c
Nikola <radanovicnikola93@gmail.com>
2018-02-04 11:43:50 +0100

    Imported random to hide our battleship in a random location on the board.

--- battleship.py
+++ battleship.py
@@ -1,3 +1,5 @@
+from random import randint
+
 board = []
 
 for i in range(5):
@@ -8,5 +10,16 @@ def print_board(board_in):
     for row in board:
         print ' '.join(row)
 
+
+def random_row(board_in):
+  return randint(0, len(board_in) - 1)
+  # or we could use return randint(0, 4)
+
+def random_col(board_in):
+  return randint(0, len(board_in) - 1)
+	# or we could use return randint(0, 4)
+
+random_row(board)
+random_col(board)
 
 print_board(board)
