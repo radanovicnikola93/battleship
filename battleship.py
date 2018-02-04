1457d14acd00467275cf852994f6e80cc8862376
Nikola <radanovicnikola93@gmail.com>
2018-02-04 12:52:27 +0100

    Added another else condition if the user had input a number not in range of the board

--- battleship.py
+++ battleship.py
@@ -27,11 +27,16 @@ ship_col = random_col(board)
 guess_row = int(raw_input('Guess Row: '))
 guess_col = int(raw_input("Guess Col: "))
 
+
 if guess_row == ship_row and guess_col == ship_col:
   print "Congratulations! You sank my battleship!"
   board[guess_row][guess_col] = "X"
   print_board(board)
 else:
-  print "You missed my battleship!"
-  board[guess_row][guess_col] = "X"
+  if guess_row not in range(5) or \
+          guess_col not in range(5):
+    print "Oops, that's not even in the ocean."
+  else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
   print_board(board)
