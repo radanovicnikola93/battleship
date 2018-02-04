4172399ae1a55b77b967c657e5dc89cca829e06f
Nikola <radanovicnikola93@gmail.com>
2018-02-04 12:43:36 +0100

    Added raw_input for the user to guess the battleship location

--- battleship.py
+++ battleship.py
@@ -10,6 +10,7 @@ def print_board(board_in):
     for row in board:
         print ' '.join(row)
 
+print_board(board)
 
 def random_row(board_in):
   return randint(0, len(board_in) - 1)
@@ -19,7 +20,18 @@ def random_col(board_in):
   return randint(0, len(board_in) - 1)
 	# or we could use return randint(0, 4)
 
-random_row(board)
-random_col(board)
+ship_row = random_row(board)
+ship_col = random_col(board)
 
-print_board(board)
+
+guess_row = int(raw_input('Guess Row: '))
+guess_col = int(raw_input("Guess Col: "))
+
+if guess_row == ship_row and guess_col == ship_col:
+  print "Congratulations! You sank my battleship!"
+  board[guess_row][guess_col] = "X"
+  print_board(board)
+else:
+  print "You missed my battleship!"
+  board[guess_row][guess_col] = "X"
+  print_board(board)
