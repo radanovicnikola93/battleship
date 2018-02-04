4ace46cbec68777daeaff1cf7f415e8ee7eada9f
Nikola <radanovicnikola93@gmail.com>
2018-02-04 13:02:03 +0100

    Added a for loop so the user has 4 guesses

--- battleship.py
+++ battleship.py
@@ -25,23 +25,25 @@ ship_col = random_col(board)
 print ship_row
 print ship_col
 
-guess_row = int(raw_input('Guess Row: '))
-guess_col = int(raw_input("Guess Col: "))
+for turn in range(4):
+    print "Turn", turn + 1
+    guess_row = int(raw_input('Guess Row: '))
+    guess_col = int(raw_input("Guess Col: "))
 
 
-if guess_row == ship_row and guess_col == ship_col:
-  print "Congratulations! You sank my battleship!"
-  board[guess_row][guess_col] = "X"
-  print_board(board)
+    if guess_row == ship_row and guess_col == ship_col:
+      print "Congratulations! You sank my battleship!"
+      board[guess_row][guess_col] = "X"
+      print_board(board)
 
-elif guess_col == 'X' and guess_row == 'X':
-    print "You guessed that one already."
+    elif guess_col == 'X' and guess_row == 'X':
+        print "You guessed that one already."
 
-else:
-  if guess_row not in range(5) or \
-          guess_col not in range(5):
-    print "Oops, that's not even in the ocean."
-  else:
-    print "You missed my battleship!"
-    board[guess_row][guess_col] = "X"
-  print_board(board)
+    else:
+      if guess_row not in range(5) or \
+              guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
+      else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+      print_board(board)
